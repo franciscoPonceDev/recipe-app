82dc0da9658f7d112171f1aef67e4f65d509ea3d
Fabrizio <fabgrel10@gmail.com>
2022-06-23 00:35:49 -0500

    Change inventories and inventories_foods resources
    Use nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :inventories, except: :update do
-    get 'foods/new', to: 'inventory_foods#new'
-    post 'foods', to: 'inventory_foods#create'
-    delete 'foods/:id', to: 'inventory_foods#destroy', as: 'food'
+    resources :inventory_foods, except: :update
   end
 
   match 'recipes/:recipe_id' => 'recipes#toogle_public', as: :toogle_public, via: :patch
