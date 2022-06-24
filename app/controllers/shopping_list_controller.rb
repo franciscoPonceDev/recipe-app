f33b6ae57942f91a64c8eb570ed085b2a8f43200
Omar Ragheb <ahmed.raph29@gmail.com>
2022-06-24 18:02:31 +0200

    Accessing the data

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,10 +1,10 @@
 class ShoppingListController < ApplicationController
   def show
-    @selected_inventory = params[:inventory_food][:inventory_id]
-    @selected_recipe = params[:recipe_id]
+    @selected_inventory = Inventory.find_by_id(params[:inventory_food][:inventory_id])
+    @selected_recipe = Recipe.find_by_id(params[:recipe_id])
 
-    @i_foods = InventoryFood.where(inventory_id: @selected_inventory).includes([:food])
-    @r_foods = RecipeFood.where(recipe_id: @selected_recipe).includes([:food])
+    @i_foods = InventoryFood.where(inventory_id: @selected_inventory.id).includes([:food])
+    @r_foods = RecipeFood.where(recipe_id: @selected_recipe.id).includes([:food])
 
     @foods = []
     @r_foods.each do |r_food|
